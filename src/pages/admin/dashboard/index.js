4263b7bfe21ae6f660d699e6d7d1429612b5c69b
Your Nameniko <285167072@qq.com>
2020-03-30 18:22:43 +0800

    updata

--- src/pages/admin/dashboard/index.js
+++ src/pages/admin/dashboard/index.js
@@ -1,23 +1,71 @@
 import React, { useState, useEffect } from "react";
+import "./index.less";
+import { Card } from "antd";
 import { mainApi } from "../../../services/dashboard";
-
 
 function Index(props) {
   const [main, setMain] = useState({});
   useEffect(() => {
     (async () => {
-        try {
-            const res = await mainApi()
-            console.log(res.data)
-            setMain(res.data)
-        }catch(err){
-            console.log(err)
-        }
-    })()
+      try {
+        const res = await mainApi();
+        console.log(res.data);
+        const main = res.data;
+        main.todayAmount = Number(main.todayAmount).toFixed(2);
+        main.orderTodayTotal = Number(main.orderTodayTotal).toFixed(2);
+        setMain(main);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
   }, []);
+  /**
+   * 因为 async 的本质是返回一个 Promise，而 useEffect 唯一接收的返回值是个函数。
+   */
+//   useEffect(async() => {
+//       try {
+//         const res = await mainApi();
+//         console.log(res.data);
+//         const main = res.data;
+//         main.todayAmount = Number(main.todayAmount).toFixed(2);
+//         main.orderTodayTotal = Number(main.orderTodayTotal).toFixed(2);
+//         setMain(main);
+//       } catch (err) {
+//         console.log(err);
+//       }
+//   }, []);
   return (
-    <div>
-      <div>今日进账:{main.todayAmount}</div>
+    <div className="dashboard">
+      <Card
+        title="今日进账"
+        style={{ width: 450, marginRight: 50 }}
+        headStyle={{ background: "#fafafa" }}
+      >
+        <div className="amount">￥{main.todayAmount}</div>
+      </Card>
+      <Card
+        title="今日订单"
+        style={{ width: 450, marginRight: 50 }}
+        headStyle={{ background: "#fafafa" }}
+      >
+        <div className="amount">￥{main.orderTodayTotal}</div>
+      </Card>
+      <Card
+        title="商品订单实时情况"
+        style={{ width: 550 }}
+        headStyle={{ background: "#fafafa" }}
+      >
+        <div className="price">
+          <div>
+            <p className="label">已支付</p>
+            <p className="count">￥{main.sumPrice}</p>
+          </div>
+          <div>
+            <p className="label">取消订单</p>
+            <p className="count">{main.cancelCount}</p>
+          </div>
+        </div>
+      </Card>
     </div>
   );
 }
